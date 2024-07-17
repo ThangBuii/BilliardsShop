1be08f324049f42dfb0ddd370ceb5b770a46d3e5
DungX <126141756+dungnot@users.noreply.github.com>
2024-07-18 03:02:58 +0700

    update

---
+++ Bi/Pages/SingleBlog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bi.Pages
+{
+    public class SingleBlogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
