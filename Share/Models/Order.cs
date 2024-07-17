ca4bcdea7e32073361e3b2caba03a30a52284a40
Thang Bui <thangbdhe170642@fpt.edu.vn>
2024-07-18 03:13:39 +0700

    Initiate done

---
+++ Share/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Share.Models
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Total { get; set; }
+        public string ShippingAddress { get; set; } = null!;
+        public int Status { get; set; }
+        public int UserIdbigint { get; set; }
+
+        public virtual User UserIdbigintNavigation { get; set; } = null!;
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
