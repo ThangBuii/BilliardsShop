384cf9d8a0cadd09c4552ddcdea3192364118527
DungX <126141756+dungnot@users.noreply.github.com>
2024-07-22 03:33:31 +0700

    add profile

---
+++ Bi/Pages/ForgetPassword/ConfirmAccount.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Client.Pages.ForgetPassword
+{
+    public class ConfirmAccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
