3262372bd0dd6f638997d04eab24d480f4697640
unknown <mittoteo0608@gmail.com>
2024-07-22 23:39:06 +0700

    Brands Admin

--- Bi/Pages/Admin/Brand/Index.cshtml.cs
+++ Bi/Pages/Admin/Brand/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Client.WebRequests;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,21 @@ namespace Client.Pages.Admin.Brand
 {
     public class IndexModel : PageModel
     {
+        private readonly ICustomHttpClient _request;
+
+        public IndexModel(ICustomHttpClient request)
+        {
+            _request = request;
+        }
+
+        [BindProperty]
+        public List<Share.Models.Brand> Brands { get; set; } = new();
+
         public void OnGet()
         {
+            Brands = new List<Share.Models.Brand>();
+            var response = _request.GetAsync("https://localhost:5000/api/Brand").Result;
+            Brands = response.Content.ReadFromJsonAsync<List<Share.Models.Brand>>().Result;
         }
     }
 }
