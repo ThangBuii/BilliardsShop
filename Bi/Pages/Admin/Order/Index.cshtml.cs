36815f2150a6e09f3bc522937a53d00141c7cbc5
unknown <mittoteo0608@gmail.com>
2024-07-23 05:22:01 +0700

    List Order Admin

--- Bi/Pages/Admin/Order/Index.cshtml.cs
+++ Bi/Pages/Admin/Order/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Client.WebRequests;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,21 @@ namespace Client.Pages.Admin.Order
 {
     public class IndexModel : PageModel
     {
+        private readonly ICustomHttpClient _request;
+
+        public IndexModel(ICustomHttpClient request)
+        {
+            _request = request;
+        }
+
+        [BindProperty]
+        public List<Share.Models.Order> Orders { get; set; } = new();
+
         public void OnGet()
         {
+            Orders = new List<Share.Models.Order>();
+            var response = _request.GetAsync("https://localhost:5000/api/Order").Result;
+            Orders = response.Content.ReadFromJsonAsync<List<Share.Models.Order>>().Result;
         }
     }
 }
