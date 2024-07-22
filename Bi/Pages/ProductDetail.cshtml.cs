409468d86795f790c72824b2077a7d8c6de09617
Thang Bui <thangbdhe170642@fpt.edu.vn>
2024-07-23 01:36:39 +0700

    Update product detail FE

--- Bi/Pages/ProductDetail.cshtml.cs
+++ Bi/Pages/ProductDetail.cshtml.cs
@@ -1,12 +1,27 @@
+using Client.WebRequests;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Share.DTO.ProductDTO;
+using Share.Models;
 
 namespace Client.Pages
 {
     public class ProductDetailModel : PageModel
     {
-        public void OnGet()
+        private readonly ICustomHttpClient _request;
+
+        [BindProperty]
+        public ProductDetailResponseDTO Product {  get; set; }
+        public ProductDetailModel(ICustomHttpClient request)
         {
+            _request = request;
+        }
+        public async Task<IActionResult> OnGetAsync([FromQuery] int id)
+        {
+            Product = new ProductDetailResponseDTO();
+            var productResponse = await _request.GetAsync($"https://localhost:5000/api/Product/Detail/{id}");
+            Product = await productResponse.Content.ReadFromJsonAsync<ProductDetailResponseDTO>();
+            return Page();
         }
     }
 }
